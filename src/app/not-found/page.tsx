e0364f23cdc0b89da1d46527203a0da750fa4af9
Yodhimas Geffananda <yodhimas02@gmail.com>
2024-10-13 22:37:56 +0700

    fix resume

---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,10 @@
+import CVNotFound from '../components/components/CVNotFound';
+
+const TimelineGamePage = () => {
+  return (
+    <CVNotFound 
+    />
+  );
+};
+
+export default TimelineGamePage;
